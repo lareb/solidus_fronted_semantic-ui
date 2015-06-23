120150356905f1f3ee8ec4a61417ef68244f70f8
stunjiturner <vision@firstsightmedia.com>
2015-06-23 15:16:21 -0400

    buttons in header

--- app/helpers/spree/frontend_helper_decorator.rb
+++ app/helpers/spree/frontend_helper_decorator.rb
@@ -32,9 +32,9 @@ Spree::FrontendHelper.module_eval do
 
   def taxons_tree(root_taxon, current_taxon, max_level = 1)
     return '' if max_level < 1 || root_taxon.children.empty?
-    content_tag :div, class: 'list-group' do
+    content_tag :div, class: 'ui list' do
       root_taxon.children.map do |taxon|
-        css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'list-group-item active' : 'list-group-item'
+        css_class = (current_taxon && current_taxon.self_and_ancestors.include?(taxon)) ? 'item active' : 'item'
         link_to(taxon.name, seo_url(taxon), class: css_class) + taxons_tree(taxon, current_taxon, max_level - 1)
       end.join("\n").html_safe
     end
